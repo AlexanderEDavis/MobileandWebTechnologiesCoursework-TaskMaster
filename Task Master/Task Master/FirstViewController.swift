0ab98ea7eaac30b01e00d1a91c66ae0825dce704
Alexander Davis <contact@alexanderdavis.tech>
2016-11-17 12:35:10 +0000

    Modification of the xCode Application
    This change makes the application a tab based application on the Swift
    iOS model

---
+++ Task Master/Task Master/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Task Master
+//
+//  Created by Alexander Davis on 17/11/2016.
+//  Copyright © 2016 Alexander Davis Computing and Media. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
